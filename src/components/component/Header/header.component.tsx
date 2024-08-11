98bf671c645b62f98789d3440257c202f70c0677
Solomon Wheeler <solly.wheeler@gmail.com>
2024-08-11 23:36:31 +0100

    Update header

--- src/components/component/Header/header.component.tsx
+++ src/components/component/Header/header.component.tsx
@@ -25,7 +25,7 @@ export default function Header() {
 
   return (
     <header className="fixed top-0 z-50 w-full bg-gray-50 dark:bg-gray-900 shadow-sm bg-opacity-100">
-      <div className="container flex h-16 items-center justify-between px-4 sm:px-6 lg:px-8">
+      <div className="w-full max-w-screen-xl mx-auto flex h-16 items-center justify-between px-4 sm:px-6 lg:px-8">
         <Link href="/" className="flex items-center gap-2" prefetch={false}>
           <HomeIcon className="h-6 w-6" />
           <span className="sr-only">Home</span>
