c1a0eb1d4eb3035db852ce5f7592c352aa96583d
Solomon Wheeler <solly.wheeler@gmail.com>
2024-08-11 20:37:22 +0100

    Add metrics

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       spacing: {
-        "1/2": "50%",
+        '1/2': '50%',
       },
       translate: {
-        "-1/2": "-50%",
+        '-1/2': '-50%',
       },
       colors: {
-        lightblue: "#ADD8E6",
+        lightblue: '#ADD8E6',
+        'light-text': '#000000', // light mode text color
+        'dark-text': '#ffffff', // dark mode text color
       },
     },
   },
-  darkMode: "class",
-  plugins: [require("tailwindcss-animate")],
+  darkMode: 'class',
+  plugins: [require('tailwindcss-animate'), require('tailwindcss-dark-mode')],
 };
 export default config;
