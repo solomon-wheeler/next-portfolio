f1c0569808a25a43f2eaaa9275d2b29357e7fd9e
Solomon Wheeler <solly.wheeler@gmail.com>
2024-08-11 18:28:23 +0100

    Update to swc

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+    '^.+\\.(js|jsx|ts|tsx)$': '@swc/jest',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
