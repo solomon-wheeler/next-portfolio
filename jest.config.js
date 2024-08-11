c5e8b1eb7a065547b90c182a7c6dc5295302ebdb
Solomon Wheeler <solly.wheeler@gmail.com>
2024-08-11 18:08:30 +0100

    Add tests

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(ts|tsx|js|jsx)$": "babel-jest",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+};
---
+++ src/components/component/SpotifyPlaying/spotifyPlaying.spec.tsx
@@ -0,0 +1,55 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import { SpotifyPlaying } from "./spotifyPlaying.component";
+import React from "react";
+import "@testing-library/jest-dom";
+
+describe("SpotifyPlaying Component", () => {
+  beforeEach(() => {
+    global.fetch = jest.fn();
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test("renders fetched data correctly", async () => {
+    const mockData = {
+      song: "Test Song",
+      artist: "Test Artist",
+      albumCover: "test-cover-url",
+    };
+
+    (global.fetch as jest.Mock).mockResolvedValue({
+      json: jest.fn().mockResolvedValue(mockData),
+    });
+
+    render(<SpotifyPlaying />);
+
+    await waitFor(() =>
+      expect(screen.getByText("🎧Currently playing")).toBeInTheDocument()
+    );
+    expect(screen.getByText("Test Song - Test Artist")).toBeInTheDocument();
+    expect(screen.getByAltText("Album Cover")).toHaveAttribute(
+      "src",
+      "test-cover-url"
+    );
+  });
+
+  test("renders equalizer bars when data is available", async () => {
+    const mockData = {
+      song: "Test Song",
+      artist: "Test Artist",
+      albumCover: "test-cover-url",
+    };
+
+    (global.fetch as jest.Mock).mockResolvedValue({
+      json: jest.fn().mockResolvedValue(mockData),
+    });
+
+    render(<SpotifyPlaying />);
+
+    await waitFor(() =>
+      expect(screen.getByText("🎧Currently playing")).toBeInTheDocument()
+    );
+  });
+});
